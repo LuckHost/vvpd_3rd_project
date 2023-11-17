03b51d9c0f8153b8ec33395887505e6196c582eb
LuckHost <sh76484@gmail.com>
2023-11-17 10:41:45 +0700

    User menu added. Input function modified

--- 3d_vvpd_project.py
+++ 3d_vvpd_project.py
@@ -1,20 +1,40 @@
 import pickle
 
-def get_correct_inp(list_of_values, menu_str = "Введите значение.. "):
-    """ returns correct input in list_of_values range"""
+def get_correct_input(value_type = "str",
+                       posbl_value = None, usr_str = "Введите значение"):
+    """ returns correct input values (int or string)"""
     while True:
-        try:
-            value = int(input(menu_str))
-            if value in list_of_values:
-                return value
-            print("Ввод инкорректен, повторите")
-            continue
-        except ValueError:
-            print("Ввод инкорректен, повторите")
+        inp_value = input(usr_str)
+        if value_type == "str" and \
+        (inp_value in posbl_value
+         or posbl_value == []):
+            return inp_value
+        if value_type == "int":
+            try:
+                int_inp_value = int(inp_value)
+                if int_inp_value in posbl_value:
+                    return int_inp_value
+            except ValueError:
+                pass
+        print("Неправильный ввод,\n Повторите попытку")
+
+def user_menu():
+    """ returns a task number """
+    while True:
+        print("Выберете действие:\n",
+              "1 - Загрузить информацию из файла\n",
+              "2 - Поиск по названию\n",
+              "3 - Фильтр по количеству остатка\n",
+              "4 - Добавить запись\n",
+              "5 - Удалить запись\n",
+              "6 - Сохранить\n",
+              "7 - Выход\n")
+        return get_correct_input("int", [1, 2, 3, 4, 5, 6, 7],
+                                "Введите номер действия: ")
 
 def main():
     """ БАКА СИНДЗИ НЕ ЛЕЗЕТ В РОБОТА """
-    print(" (͡° ͜ʖ ͡°)  А мне арбузы дороже комплементации человечества..")
+    print(user_menu())
 
 if __name__ == "__main__":
     main()
