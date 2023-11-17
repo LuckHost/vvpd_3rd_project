27267eb292e549b0d4f19a9da36343b80e8a3acb
LuckHost <sh76484@gmail.com>
2023-11-17 10:36:53 +0700

    Correct input added

--- 3d_vvpd_project.py
+++ 3d_vvpd_project.py
@@ -1,5 +1,17 @@
 import pickle
 
+def get_correct_inp(list_of_values, menu_str = "Введите значение.. "):
+    """ returns correct input in list_of_values range"""
+    while True:
+        try:
+            value = int(input(menu_str))
+            if value in list_of_values:
+                return value
+            print("Ввод инкорректен, повторите")
+            continue
+        except ValueError:
+            print("Ввод инкорректен, повторите")
+
 def main():
     """ БАКА СИНДЗИ НЕ ЛЕЗЕТ В РОБОТА """
     print(" (͡° ͜ʖ ͡°)  А мне арбузы дороже комплементации человечества..")
